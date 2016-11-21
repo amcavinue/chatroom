0689da90e08b7704621b1e01aa51350ca23ba1a5
Aaron <aaronmcavinue@gmail.com>
2016-11-21 22:56:46 +0000

    Add functionality for streaming messages, broadcasting when users log on/off, and displaying the num users currently on.

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+    var socket = io();
     var input = $('input');
     var messages = $('#messages');
+    var notifications = $('.notifications');
+    var usersOnline = $('.num-online');
 
     var addMessage = function(message) {
         messages.append('<div>' + message + '</div>');
@@ -13,6 +16,18 @@ $(document).ready(function() {
 
         var message = input.val();
         addMessage(message);
+        socket.emit('message', message);
         input.val('');
+    });
+    
+    socket.on('onlineUsers', function(numUsers) {
+        usersOnline.empty().html(numUsers);
+    });
+    
+    socket.on('message', addMessage);
+    
+    socket.on('notification', function(notification) {
+        notifications.empty().html(notification);
+        notifications.show().delay(3000).fadeOut();
     });
 });
--- server.js
+++ server.js
@@ -1,5 +1,37 @@
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
 
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+var usersOnline = 0;
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+    
+    usersOnline++;
+    
+    socket.on('message', function(message) {
+        console.log('Received message:', message);
+        socket.broadcast.emit('message', message);
+    });
+    
+    socket.on('disconnect', function () {
+        usersOnline--;
+        socket.broadcast.emit('notification', 'Another user disconnected.');
+        socket.broadcast.emit('onlineUsers', usersOnline);
+    });
+    
+    socket.broadcast.emit('notification', 'New user online.');
+    socket.broadcast.emit('onlineUsers', usersOnline);
+    
+    // Sends the data to the client immediately.
+    socket.emit('notification', 'New user online.');
+    socket.emit('onlineUsers', usersOnline);
+});
+
+server.listen(process.env.PORT || 8080);
